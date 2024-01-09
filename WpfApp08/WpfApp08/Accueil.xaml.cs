40c7e40628aadba1d5369bb1d358de0ee908aa10
Matheol08 <matheo.lesenne@viacesi.fr>
2024-01-09 09:02:25 +0100

    debut du front

---
+++ WpfApp08/WpfApp08/Accueil.xaml.cs
@@ -0,0 +1,45 @@
+﻿
+using System.Windows;
+
+namespace WpfApp08
+{
+    public partial class Accueil : Window
+    {
+        public Accueil()
+        {
+            InitializeComponent();
+            // Centrer la fenêtre
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+            // Rendre la fenêtre non redimensionnable
+            ResizeMode = ResizeMode.NoResize;
+        }
+
+        private void RetourButton_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 PageAcceuil = new Window1();
+            PageAcceuil.Show();
+            this.Close();
+        }
+        private void Button_site(object sender, RoutedEventArgs e)
+        {
+            CRUDSpecialite Pagesite = new CRUDSpecialite();
+            Pagesite.Show();
+            this.Close();
+        }
+        private void Button_service(object sender, RoutedEventArgs e)
+        {
+            CRUDMedecin Pagesite = new CRUDMedecin();
+            Pagesite.Show();
+            this.Close();
+        }
+        private void Button_utilisateur(object sender, RoutedEventArgs e)
+        {
+            CRUDRendezVous PageEmploye = new CRUDRendezVous();
+            PageEmploye.Show();
+            this.Close();
+        }
+
+       
+    }
+}
